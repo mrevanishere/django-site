e3a168a2468636fd60c45f7b03a69e56ff25d61a
Evan <mrevanishere@gmail.com>
2021-08-28 23:16:43 -0700

    removed users app

--- psite/psite/settings.py
+++ psite/psite/settings.py
@@ -41,7 +41,6 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'blog',
-    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- psite/psite/urls.py
+++ psite/psite/urls.py
@@ -21,26 +21,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 """
-
-
-
-# import os
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-#adminURL = ''
-#base_path = os.path.dirname(__file__)
-#file_path = os.path.abspath(os.path.join(base_path, "..", "..", "..", "admin.txt"))
-#with open(file_path, "r") as f:
-#    adminURL = f.read().strip()
 
 urlpatterns = [
                   path('', include('blog.urls')),
                   path('admin/', admin.site.urls),
-                  path('account/', include('user.urls')),
-                  # path('accounts/', include('django.contrib.auth.urls')),
               ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
